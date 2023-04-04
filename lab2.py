4b4868d6d9de65a771d5fe8b07c3d200be5c42b4
Robert Tetreault <rrt2850@g.rit.edu>
2023-04-04 17:48:28 -0400

    got the solver working for basic non-function cases

--- lab2.py
+++ lab2.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Dict
 
 kb = {
     'predicates': [],
@@ -8,69 +9,11 @@ kb = {
     'clauses': []
 }
 
-facts = []
-
-class CompoundPredicate:
-    def addToFacts(self):
-        if self not in facts:
-            facts.append(self)
-
-    def evaluate(self):
-        if isinstance(self.predicate1, CompoundPredicate):
-            left = self.predicate1.evaluate()
-        else:
-            left = evaluate(self.predicate1)
-
-        if isinstance(self.predicate2, CompoundPredicate):
-            right = self.predicate2.evaluate()
-        else:
-            right = evaluate(self.predicate2)
-
-        match self.joinOp:
-            case "and":
-                return left and right
-            case "or":
-                return left or right
-            case _:
-                raise ValueError("invalid join operator")
-
-    def __init__(self, predicate1, joinOp, predicate2):
-        self.predicate1 = predicate1
-        self.predicate2 = predicate2
-        self.joinOp = joinOp
-
-    def __eq__(self, other):
-        return (isinstance(other, self.__class__)
-                and self.predicate1 == other.predicate1
-                and self.predicate2 == other.predicate2
-                and self.joinOp == other.joinOp)
-
-    def negate(self):
-        self.predicate1 = self.predicate1.negate()
-        self.predicate2 = self.predicate2.negate()
-
-    def __repr__(self):
-        if isinstance(self.predicate1, CompoundPredicate):
-            left = f"({self.predicate1})"
-        else:
-            left = str(self.predicate1)
-
-        if isinstance(self.predicate2, CompoundPredicate):
-            right = f"({self.predicate2})"
-        else:
-            right = str(self.predicate2)
-
-        return f"{left} {self.joinOp} {right}"
-
 
 class Predicate:
     def negate(self):
         self.negated = not self.negated
         return self
-
-    def addToFacts(self):
-        if self not in facts:
-            facts.append(self)
 
     def __init__(self, predicate, variables):
         self.predicate = predicate
@@ -82,19 +25,10 @@ class Predicate:
                 and self.predicate == other.predicate
                 and self.variables == other.variables
                 and self.negated == other.negated)
-    
-    def __and__(self, other):
-        return evaluate(self) and evaluate(other)
-    
-    def __or__(self, other):
-        return evaluate(self) or evaluate(other)
-    
+
     def __repr__(self):
         return '{}{}({})'.format('!' if self.negated else '', self.predicate, ', '.join(self.variables))
 
-def evaluate(predicate):
-        # Evaluate the truth value of the predicate in the given fact table
-        return predicate in facts
 
 def toPredicate(s):
     negated = False
@@ -104,11 +38,12 @@ def toPredicate(s):
 
     i = s.find('(')
     predicate = s[:i]
-    variables = []
-    for v in s[i+1:-1].split(','): variables.append(v.strip())
+    variables = [v.strip() for v in s[i + 1:-1].split(',')]
 
-    if negated: return Predicate(predicate, variables).negate()
+    if negated:
+        return Predicate(predicate, variables).negate()
     return Predicate(predicate, variables)
+
 
 def toPredicates(clause):
     clause = clause.split(" ")
@@ -119,16 +54,45 @@ def toPredicates(clause):
 
     return temp
 
-def clauseToCompound(clause, op):
-    if isinstance(clause, Predicate):
-        return clause
-    while len(clause) > 1:
-        pred1 = clause.pop(0)
-        pred2 = clause.pop(0)
-        compound = CompoundPredicate(pred1, op, pred2)
-        clause.insert(0, compound)
-    return clause[0]
+def unify_terms(term1: str, term2: str, substitutions: Dict[str, str]) -> Dict[str, str]:
+    if term1 == term2:
+        return substitutions
+    if term1 in kb['variables']:
+        return unify_variable(term1, term2, substitutions)
+    if term2 in kb['variables']:
+        return unify_variable(term2, term1, substitutions)
+    return None
 
+def unify_variable(var: str, x: str, substitutions: Dict[str, str]) -> Dict[str, str]:
+    if var in substitutions:
+        return unify_terms(substitutions[var], x, substitutions)
+    if x in substitutions:
+        return unify_terms(var, substitutions[x], substitutions)
+    substitutions[var] = x
+    return substitutions
+
+def unify(p1: Predicate, p2: Predicate) -> Dict[str, str]:
+    if p1.predicate != p2.predicate or len(p1.variables) != len(p2.variables):
+        return None
+
+    substitutions = {}
+    for term1, term2 in zip(p1.variables, p2.variables):
+        substitutions = unify_terms(term1, term2, substitutions)
+        if substitutions is None:
+            return None
+
+    return substitutions
+
+def resolution(c1, c2):
+    for literal1 in c1:
+        neg_literal1 = literal1.negate()
+        for literal2 in c2:
+            substitutions = unify(neg_literal1, literal2)
+            if substitutions is not None:
+                new_clause = [l for l in c1 if l != literal1] + [l for l in c2 if l != literal2]
+                new_clause = [Predicate(p.predicate, [substitutions.get(v, v) for v in p.variables]) for p in new_clause]
+                return new_clause
+    return None
 
 def main():
     with open(sys.argv[1], 'r') as file:
@@ -146,32 +110,36 @@ def main():
             elif words[0] != 'Clauses:':
                 kb['clauses'].append(line.strip())
 
-    
-    # convert all the predicates to Predicate objects
     clauses = [toPredicates(c) for c in kb['clauses']]
-    kb['clauses'] = clauses
 
-    # iterate over the clauses
-    for clause in clauses[:]:
-        # check if the clause has a single predicate
-        if len(clause) == 1:
-            predicate = clause[0]
-            # check if the fact is already in the fact table
-            if predicate not in facts:
-                if (predicate.negated) in facts: # check if the new fact contradicts any existing facts
-                    print("no")
-                    return
-                # if the fact is not in the fact table, add it
-                predicate.addToFacts()
-            # create a new list with the remaining clauses
-            clauses.remove(clause)
-    clauses = [clauseToCompound(c, 'and') for c in clauses]
-    superPredicate = clauseToCompound(clauses, 'or')
-    factPred = clauseToCompound(facts, 'and')
-    finalCompound = CompoundPredicate(factPred, 'or', superPredicate)
-    print(finalCompound)
-    print("yes" if finalCompound.evaluate() else "no")
+    resolved = set()
+    while True:
+        new_clauses = []
+        for i in range(len(clauses)):
+            for j in range(i+1, len(clauses)):
+                if (i, j) not in resolved:
+                    new_clause = resolution(clauses[i], clauses[j])
+                    if new_clause is not None:
+                        new_clauses.append(new_clause)
+                        resolved.add((i, j))
 
+        if not new_clauses:
+            print("yes")  # The knowledge base is satisfiable
+            break
+
+        empty_clause_found = False
+        for new_clause in new_clauses:
+            # Check for contradictions (empty clauses)
+            if not new_clause:
+                print("no")  # The knowledge base is not satisfiable
+                empty_clause_found = True
+                break
+
+            if new_clause not in clauses:
+                clauses.append(new_clause)
+
+        if empty_clause_found:
+            break
 
 if __name__ == '__main__':
     main()
